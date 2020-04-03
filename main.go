0f8a268803d5deca7cc001bca1e188ef4106759e
y-shika <shikanai327327@gmail.com>
2020-04-03 18:08:36 +0900

    update: json形式でレスポンスを返すようにした

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type fakeGithubData struct {
+	Login     string `json:"login"`
+	ID        int    `json:"id"`
+	AvatarURL string `json:"avatarUrl"`
+}
+
+func fakeGithubAPIHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := fakeGithubData{
+			Login:     "y-shika",
+			ID:        1,
+			AvatarURL: "https://avatars3.githubusercontent.com/u/583231?v=4",
+		}
+
+		res, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, string(res))
+	}
+}
 
 func helloHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +48,7 @@ func main() {
 
 	r := mux.NewRouter()
 
+	r.Handle("/api/github", fakeGithubAPIHandler()).Methods(http.MethodGet)
 	r.Handle("/hello", helloHandler()).Methods(http.MethodGet)
 
 	r.HandleFunc("/world", world)
