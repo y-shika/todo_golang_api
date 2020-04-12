fae2e9f5b3eb2fffc8ab04da42258bf072add252
y-shika <shikanai327327@gmail.com>
2020-04-12 17:13:49 +0900

    clean: コメント整理

--- db/mysql_init/1_create_db.sql
+++ db/mysql_init/1_create_db.sql
@@ -1,14 +1,12 @@
--- DROP DATABASE IF EXISTS todo_golang_api;
 CREATE DATABASE IF NOT EXISTS todo_golang_api;
 USE todo_golang_api;
--- DROP TABLE IF EXISTS todos;
 
 CREATE TABLE IF NOT EXISTS todos (
-    -- idはuuid v4を使っているため
+    -- idはuuid v4を使っているため36としている
     id varchar(36) primary key,
     title text,
     active boolean,
     detail text
 );
 
-INSERT INTO todos VALUES ("1", "Title_1", true, "Detail_1"), ("2", "Title_2", true, "Detail_2");
+INSERT INTO todos VALUES ("1", "Title_1", true, "Detail_1"), ("2", "Title_2", true, "Detail_2"), ("3", "Title_3", true, "Detail_3");
